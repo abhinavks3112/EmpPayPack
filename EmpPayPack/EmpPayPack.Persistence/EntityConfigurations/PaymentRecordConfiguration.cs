94bf1f4a39c64ac6d7094d8c028a76d782d7e7ec
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2020-03-19 13:58:03 +0530

    PaymentRecordConfiguration: One To One or Optional relationship between Tax Year and Payment Record added

--- EmpPayPack/EmpPayPack.Persistence/EntityConfigurations/PaymentRecordConfiguration.cs
+++ EmpPayPack/EmpPayPack.Persistence/EntityConfigurations/PaymentRecordConfiguration.cs
@@ -7,14 +7,14 @@ using System.Text;
 
 namespace EmpPayPack.Persistence.EntityConfigurations
 {
-    public class PayRecordConfiguration: IEntityTypeConfiguration<PaymentRecord>
+    public class PaymentRecordConfiguration: IEntityTypeConfiguration<PaymentRecord>
     {
         private const string COLUMN_TYPE_MONEY = "Money";
         private const string COLUMN_TYPE_DECIMAL_WITH_PRECISION_18_2 = "decimal(18,2)";
         public void Configure(EntityTypeBuilder<PaymentRecord> builder)
         {
             #region -----Foreign Key and Relationship------------
-            
+
             /*
              * One Employee can have multiple payment records so configuring One To Many relationship between Employee and PaymentRecords respectively
              * Here, Principal: Employee, Dependent: Payment Records, Navigation Property in Payment Records: Employee, Foreign Key: EmployeeId
@@ -23,10 +23,14 @@ namespace EmpPayPack.Persistence.EntityConfigurations
                 .WithMany(e => e.PaymentRecords)
                 .HasForeignKey(p => p.EmployeeId);
             /*
-            * One Payment Record must have one tax year specified for it so configuring One To One relationship between Payment Record and PaymentRecords respectively
+            * One Payment Record must have one tax year specified for it so configuring One To One relationship between Payment Record and PaymentRecords respectively.
+            * Also, deleting a Payment Record should not delete the Tax Year
            */
             builder.HasOne(p => p.TaxYear)
-                .WithOne().IsRequired();
+                .WithOne()
+                .HasForeignKey<PaymentRecord>(p => p.TaxYearId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.SetNull);
 
             #endregion
 
