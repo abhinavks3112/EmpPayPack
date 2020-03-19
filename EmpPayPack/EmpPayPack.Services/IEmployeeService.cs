54e35e73167a7f067a04c7cb25b1a8f5834aabf9
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2020-03-19 17:18:00 +0530

    Services Layer: Created IEmployeeService interface and implemented it in EmployeeService class

---
+++ EmpPayPack/EmpPayPack.Services/IEmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EmpPayPack.Entity;
+
+namespace EmpPayPack.Services
+{
+    public interface IEmployeeService
+    {
+        Task CreateASync(Employee newEmployee);
+        Employee GetById(int employeeId);
+        Task UpdateAsync(Employee employee);
+        Task UpdateAsync(int employeeId);
+        Task DeleteAsync(int employeeId);
+        decimal UnionFees(int employeeId);
+        decimal StudentLoanRepaymentAmount(int id, decimal totalAmount);
+        IEnumerable<Employee> GetAll();
+    }
+}
---
+++ EmpPayPack/EmpPayPack.Services/Implementation/EmployeeService.cs
@@ -0,0 +1,53 @@
+﻿using EmpPayPack.Entity;
+using EmpPayPack.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpPayPack.Services.Implementation
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeeService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateASync(Employee newEmployee)
+        {
+            await _context.Employees.AddAsync(newEmployee);
+            await _context.SaveChangesAsync();
+        }
+        public Employee GetById(int employeeId)
+            => _context.Employees.Where(e => e.Id == employeeId).FirstOrDefault();
+
+        public async Task DeleteAsync(int employeeId)
+        {  
+            _context.Employees.Remove(GetById(employeeId));
+             await _context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(Employee employee)
+        {
+            _context.Employees.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(int employeeId)
+        {
+            _context.Employees.Update(GetById(employeeId));
+            await _context.SaveChangesAsync();
+        }
+        public IEnumerable<Employee> GetAll() => _context.Employees;
+        public decimal StudentLoanRepaymentAmount(int id, decimal totalAmount)
+        {
+         throw new NotImplementedException();
+        }
+
+        public decimal UnionFees(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
