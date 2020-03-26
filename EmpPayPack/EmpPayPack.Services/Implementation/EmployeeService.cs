4aadc688c9a0a14f55b83d429d78c6019ef4a92c
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2020-03-26 17:01:42 +0530

    Services Layer: Employee's Student Loan Repayment and Union Fee's calculations added

--- EmpPayPack/EmpPayPack.Services/Implementation/EmployeeService.cs
+++ EmpPayPack/EmpPayPack.Services/Implementation/EmployeeService.cs
@@ -11,6 +11,8 @@ namespace EmpPayPack.Services.Implementation
     public class EmployeeService : IEmployeeService
     {
         private readonly ApplicationDbContext _context;
+        private decimal _studenLoanAmount;
+
         public EmployeeService(ApplicationDbContext context)
         {
             _context = context;
@@ -42,12 +44,31 @@ namespace EmpPayPack.Services.Implementation
         public IEnumerable<Employee> GetAll() => _context.Employees;
         public decimal StudentLoanRepaymentAmount(int id, decimal totalAmount)
         {
-         throw new NotImplementedException();
+            var employee = GetById(id);
+            _studenLoanAmount = 0;
+            if (employee.StudentLoan == StudentLoan.Yes)
+            {
+                if(totalAmount > 1750 && totalAmount <= 2000)
+                {
+                    _studenLoanAmount = 15m;
+                }
+                else if(totalAmount > 2000 && totalAmount <= 2250)
+                {
+                    _studenLoanAmount = 38m;
+                }
+                else if (totalAmount > 2250 && totalAmount <= 2500)
+                {
+                    _studenLoanAmount = 60m;
+                }
+                else if (totalAmount > 2500 && totalAmount <= 2250)
+                {
+                    _studenLoanAmount = 83m;
+                }
+            }
+            return _studenLoanAmount;
         }
 
-        public decimal UnionFees(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public decimal UnionFees(int id) => GetById(id).UnionMember == UnionMember.Yes ? 10m : 0m;
+        
     }
 }
