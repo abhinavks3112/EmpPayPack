f74689711f62b5a082d461601098fa4fe38bf66d
Abhinav <abhiks3112@gmail.com>
2020-04-03 15:23:15 +0530

    Presentation Layer: Pagination generic class added

---
+++ EmpPayPack/EmpPayPack/EmployeeListPagination.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmpPayPack
+{
+    public class EmployeeListPagination<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public EmployeeListPagination(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            this.AddRange(items);
+        }
+
+        // Enable/Disable Previous paging button
+        public bool IsPreviousPageAvailable => PageIndex > 1;
+
+        // Enable/Disable Next paging button
+        public bool IsNextPageAvailable => PageIndex < TotalPages;
+
+        public static EmployeeListPagination<T> Create(IList<T> source, int pageIndex, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+            return new EmployeeListPagination<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
